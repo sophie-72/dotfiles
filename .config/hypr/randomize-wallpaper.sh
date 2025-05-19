ac04e3743df8608fa4a5b5ad1788414da379af25
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-05-19 10:28:01 -0400

    randomize wallpaper

---
+++ .config/hypr/randomize-wallpaper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sleep 3
+
+WALLPAPER_DIR="$HOME/wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
+
+echo "Selected wallpaper: $WALLPAPER" >> /tmp/randomize-wallpaper.log
