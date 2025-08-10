5b285bbe6d02a4fbbd78b8d9e664850f5ee9f221
Sophie <65089742+sophie-72@users.noreply.github.com>
2025-08-10 16:38:17 -0400

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ if ! pacman -Qi paru > /dev/null; then
     cd ..
 fi
 
-echo "Installing required packages"
+echo -e "\nInstalling required packages"
 paru -S --needed curl zsh vim neovim nodejs npm papirus-icon-theme sddm hyprland hyprpaper hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent udiskie waybar rofi-wayland rofi-power-menu dunst kitty qt5-base qt5-graphicaleffects qt5-quick3d qt5-quickcontrols qt5-quickcontrols2 otf-font-awesome grim slurp wl-clipboard
 
-echo "Installing oh-my-zsh and plugins"
+echo -e "\nInstalling oh-my-zsh and plugins"
+echo "IMPORTANT: Type 'exit' after the Oh My Zsh installation to allow this script to continue."
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -20,10 +21,10 @@ if [ ! -d ~/.oh-my-zsh ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
-echo "Installing vim-plug for neovim"
+echo -e "\nInstalling vim-plug for neovim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo "Copying SDDM config files"
+echo -e "\nCopying SDDM config files"
 sudo mkdir -p /etc/sddm.conf.d/
 sudo cp .config/sddm/default.conf /etc/sddm.conf.d/
 [ ! -d "sddm-rose-pine" ] && git clone https://github.com/lwndhrst/sddm-rose-pine.git
